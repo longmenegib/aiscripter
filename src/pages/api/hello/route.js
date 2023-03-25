a4ca0d3f9a731173aac1cd916bc02117d8b53e3e
longmene gibril <longmenegib@longmenegib.domain.local>
2023-03-25 12:13:28 +0100

    terminer avec l site

--- src/pages/api/hello/route.js
+++ src/pages/api/hello/route.js
@@ -15,18 +15,19 @@ const handler = async(req, res) => {
     case 'POST':
       await authenticate(req, res);
       if (req.body.prompt !== undefined) {
-        const completion = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt: `${req.body.prompt}`,
-          temperature: .7,
-          max_tokens: 2048,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-          stop: ["{}"],
-        });
+        // const completion = await openai.createCompletion({
+        //   model: "text-davinci-003",
+        //   prompt: `${req.body.prompt}`,
+        //   temperature: .7,
+        //   max_tokens: 2048,
+        //   top_p: 1,
+        //   frequency_penalty: 0,
+        //   presence_penalty: 0,
+        //   stop: ["{}"],
+        // });
 
-        res.status(200).json({ text: completion.data.choices[0].text, success: true });
+        // res.status(200).json({ text: completion.data.choices[0].text, success: true });
+        res.status(200).json({ message: 'post eee',  success: true})
       }
       else {
         res.status(400).json({ text: "No prompt provided.", success: false });
